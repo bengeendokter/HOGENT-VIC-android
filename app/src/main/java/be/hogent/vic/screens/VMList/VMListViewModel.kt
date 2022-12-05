b31edfe0bd3ec12227ee7f39d4c6da1c8792831a
aglim-benli <aglim.benli@student.hogent.be>
2022-12-05 15:47:07 +0100

    Add vm list view model

--- app/src/main/java/be/hogent/vic/screens/VMList/VMListViewModel.kt
+++ app/src/main/java/be/hogent/vic/screens/VMList/VMListViewModel.kt
@@ -1,4 +1,32 @@
 package be.hogent.vic.screens.VMList
 
-class VMListViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import be.hogent.vic.database.getDatabase
+import be.hogent.vic.repository.VirtualMachineRepository
+import kotlinx.coroutines.launch
+
+class VMListViewModel(application: Application) : AndroidViewModel(application) {
+    private val database = getDatabase(application)
+    private val virtualMachineRepository = VirtualMachineRepository(database)
+    val virtualMachines = virtualMachineRepository.virtualMachines
+
+    init {
+        viewModelScope.launch {
+            virtualMachineRepository.refreshVirtualMachines()
+        }
+    }
+
+    class Factory(val app: Application) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(VMListViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return VMListViewModel(app) as T
+            }
+            throw IllegalArgumentException("Unable to construct viewmodel")
+        }
+    }
 }
