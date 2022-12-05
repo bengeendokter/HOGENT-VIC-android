6e0898f735556475562c19395502bebb09261e55
aglim-benli <aglim.benli@student.hogent.be>
2022-12-05 14:47:07 +0100

    Add vm enums & client

---
+++ app/src/main/java/be/hogent/vic/domain/Client.kt
@@ -0,0 +1,12 @@
+package be.hogent.vic.domain
+
+import java.util.Date
+
+data class Client(
+    // From index dto
+    val id: Int,
+    val name: String,
+    // TODO...
+) {
+
+}
--- app/src/main/java/be/hogent/vic/domain/VirtualMachine.kt
+++ app/src/main/java/be/hogent/vic/domain/VirtualMachine.kt
@@ -13,18 +13,31 @@ data class VirtualMachine(
     val endDate: Date,
     val isActive: Boolean,
     val isHighlyAvailable: Boolean,
-    // val template: String,
-    // val backupFrequency: String,
+    val template: Template,
+    val backupFrequency: BackupFrequency,
 
     // From detail dto
     val hostName: String? = null,
     val fqdn: String? = null,
     val host: String? = null,
     val ports: String? = null,
-    // val client: String? = null,
-    // val availability: String? = null,
-    // val mode: String? = null,
+    val client: Client? = null,
+    val availability: Day? = null,
+    val mode: Mode? = null,
     val createdAt: Date? = null
 ) {
 
+}
+
+enum class Template { ARTIFICIAL_INTELLIGENCE, DATABASE, MACHINE_LEARNING }
+enum class BackupFrequency { DAILY, WEEKLY }
+enum class Mode { IAAS, PAAS, SAAS }
+enum class Day(val value: Int) {
+    MONDAY(1),
+    TUESDAY(2),
+    WEDNESDAY(4),
+    THURSDAY(8),
+    FRIDAY(16),
+    SATURDAY(32),
+    SUNDAY(64)
 }
