629fc1ead44b4026dfc3f67620f41f07c6a4484a
aglim-benli <aglim.benli@student.hogent.be>
2022-12-05 15:01:06 +0100

    Add vm db dto

--- app/src/main/java/be/hogent/vic/database/VirtualMachineDatabaseDto.kt
+++ app/src/main/java/be/hogent/vic/database/VirtualMachineDatabaseDto.kt
@@ -1,10 +1,58 @@
 package be.hogent.vic.database
 
 import androidx.room.*
+import be.hogent.vic.domain.*
+import java.util.*
 
-@Dao
-interface VirtualMachineDatabaseDto {
+@Entity(tableName = "virtual_machines")
+data class VirtualMachineDatabaseDto constructor(
+    // From index dto
+    @PrimaryKey
+    val id: Int,
+    val name: String,
+    val cpu: Int,
+    val ram: Int,
+    val storage: Int,
+    val startDate: Date,
+    val endDate: Date,
+    val isActive: Boolean,
+    val isHighlyAvailable: Boolean,
+    val template: Template,
+    val backupFrequency: BackupFrequency,
 
-//    @Query("SELECT * from custom_joke_table WHERE Id = :key")
-//    suspend fun get(key: Int): DatabaseVirtualMachine?
+    // From detail dto
+    val hostName: String? = null,
+    val fqdn: String? = null,
+    val host: String? = null,
+    val ports: String? = null,
+    val client: Client? = null,
+    val availability: Day? = null,
+    val mode: Mode? = null,
+    val createdAt: Date? = null
+)
+
+fun List<VirtualMachineDatabaseDto>.asDomainModel(): List<VirtualMachine> {
+    return map {
+        VirtualMachine(
+            id = it.id,
+            name = it.name,
+            cpu = it.cpu,
+            ram = it.ram,
+            storage = it.storage,
+            startDate = it.startDate,
+            endDate = it.endDate,
+            isActive = it.isActive,
+            isHighlyAvailable = it.isHighlyAvailable,
+            template = it.template,
+            backupFrequency = it.backupFrequency,
+            hostName = it.hostName,
+            fqdn = it.fqdn,
+            host = it.host,
+            ports = it.ports,
+            client = it.client,
+            availability = it.availability,
+            mode = it.mode,
+            createdAt = it.createdAt
+        )
+    }
 }
