8f7a0a88df6e05770fbe7dc8a89bce3b2a79f6d2
aglim-benli <aglim.benli@student.hogent.be>
2022-12-05 16:53:57 +0100

    Add json date adapter

--- app/src/main/java/be/hogent/vic/network/Service.kt
+++ app/src/main/java/be/hogent/vic/network/Service.kt
@@ -2,11 +2,13 @@ package be.hogent.vic.network
 
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.Deferred
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import java.util.*
 
 interface VicService {
     @GET("/virtualmachine")
@@ -15,6 +17,7 @@ interface VicService {
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
+    .add(Date::class.java, Rfc3339DateJsonAdapter().nullSafe())
     .build()
 
 object Network {
