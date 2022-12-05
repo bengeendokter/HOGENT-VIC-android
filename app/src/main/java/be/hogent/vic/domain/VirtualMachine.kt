84a5722ede9a090ca2841538c3d55253d7f3b848
aglim-benli <aglim.benli@student.hogent.be>
2022-12-05 14:21:03 +0100

    Update vm domain model

--- app/src/main/java/be/hogent/vic/domain/VirtualMachine.kt
+++ app/src/main/java/be/hogent/vic/domain/VirtualMachine.kt
@@ -4,19 +4,25 @@ import java.time.DateTimeException
 import java.util.Date
 
 data class VirtualMachine(
-    var Id: Int = 1,
-    var Name: String = "",
-    var hostName: String = "",
-    var host: String = "",
-    var fqdn: String = "",
-    var cpu: Int = 0,
-    var ram: Int = 0,
-    var storage: Int = 0,
-//    var poorten: String = "",
-//    var template: String = "",
-    var mode: String = "",
-    var startDate: Date = Date(),
-    var endDate: Date = Date(),
+    val id: Int,
+    val name: String,
+    val hostName: String,
+    val host: String,
+    val fqdn: String,
+    val cpu: Int,
+    val ram: Int,
+    val storage: Int,
+    val ports: String,
+    // val template: String,
+    // val mode: String,
+    // val availability: String,
+    // val backupFrequency: String,
+    // val client: String,
+    val isActive: Boolean,
+    val isHighlyAvailable: Boolean,
+    val startDate: Date,
+    val endDate: Date,
+    val createdAt: Date
 ) {
 
 }
