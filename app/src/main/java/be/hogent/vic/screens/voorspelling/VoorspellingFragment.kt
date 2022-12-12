21bfad081b2a8c75a42ed1b844d18ce887014f79
Mert Sahin <mert.sahin@student.hogent.be>
2022-12-12 17:35:23 +0100

    voorspelling gebruikt echte data, geen mock data

--- app/src/main/java/be/hogent/vic/screens/voorspelling/VoorspellingFragment.kt
+++ app/src/main/java/be/hogent/vic/screens/voorspelling/VoorspellingFragment.kt
@@ -11,6 +11,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import be.hogent.vic.R
 import be.hogent.vic.databinding.FragmentVoorspellingBinding
+import be.hogent.vic.screens.virtualmachinelist.VirtualMachineListViewModel
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.Calendar
@@ -18,7 +19,16 @@ import java.util.Date
 
 class VoorspellingFragment : Fragment() {
     private lateinit var binding: FragmentVoorspellingBinding
-    private lateinit var viewModel: VoorspellingViewModel
+    private val viewModel: VoorspellingViewModel by lazy {
+        val activity = requireNotNull(this.activity) {
+            "You can only access the viewModel after onViewCreated()"
+        }
+
+        ViewModelProvider(
+            this,
+            VoorspellingViewModel.Factory(activity.application)
+        ).get(VoorspellingViewModel::class.java)
+    }
 
     var datum: Date = Date()
 
@@ -27,7 +37,7 @@ class VoorspellingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentVoorspellingBinding.inflate(inflater)
-        viewModel = ViewModelProvider(this).get(VoorspellingViewModel::class.java)
+        //viewModel = ViewModelProvider(this).get(VoorspellingViewModel::class.java)
 
         binding.voorspelling = viewModel
         binding.lifecycleOwner = this
@@ -51,24 +61,28 @@ class VoorspellingFragment : Fragment() {
                 day
             )
 
-           val minDay = c.get(Calendar.DAY_OF_MONTH)
-           val minMonth = c.get(Calendar.MONTH)
-           val minYear = c.get(Calendar.YEAR)
-           c.set(minYear, minMonth, minDay)
-           datePickerDialog.datePicker.minDate = c.timeInMillis
+            val minDay = c.get(Calendar.DAY_OF_MONTH)
+            val minMonth = c.get(Calendar.MONTH)
+            val minYear = c.get(Calendar.YEAR)
+            c.set(minYear, minMonth, minDay)
+            datePickerDialog.datePicker.minDate = c.timeInMillis
 
-           val maxDay = c.get(Calendar.DAY_OF_MONTH)
-           val maxMonth = c.get(Calendar.MONTH)
-           val maxYear = c.get(Calendar.YEAR).plus(1)
-           c.set(maxYear, maxMonth, maxDay)
-           datePickerDialog.datePicker.maxDate = c.timeInMillis
+            val maxDay = c.get(Calendar.DAY_OF_MONTH)
+            val maxMonth = c.get(Calendar.MONTH)
+            val maxYear = c.get(Calendar.YEAR).plus(3)
+            c.set(maxYear, maxMonth, maxDay)
+            datePickerDialog.datePicker.maxDate = c.timeInMillis
 
-           datePickerDialog.show()
+            datePickerDialog.show()
 
         }
 
         binding.voorspellingDatum.text =  SimpleDateFormat("dd/MM/yyyy").format(datum).toString()
-        viewModel.doeVoorspelling(datum)
+        //viewModel.doeVoorspelling(datum)
+
+        viewModel.vms.observe(viewLifecycleOwner, Observer {
+            //lijst -> binding.vie  = lijst
+        })
 
         return binding.root
     }
--- app/src/main/java/be/hogent/vic/screens/voorspelling/VoorspellingViewModel.kt
+++ app/src/main/java/be/hogent/vic/screens/voorspelling/VoorspellingViewModel.kt
@@ -1,61 +1,21 @@
 package be.hogent.vic.screens.voorspelling
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.*
+import be.hogent.vic.database.getDatabase
 import be.hogent.vic.domain.BackupFrequency
 import be.hogent.vic.domain.Template
 import be.hogent.vic.domain.VirtualMachine
 import be.hogent.vic.domain.Voorspelling
+import be.hogent.vic.repository.VirtualMachineRepository
+import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.*
 
-class VoorspellingViewModel: ViewModel() {
-    private val vms = listOf(
-        VirtualMachine(
-            1, "VM-IT-1",
-            4, 3, 950,
-            SimpleDateFormat("yyyy/MM/dd").parse("2022/11/01"),
-            SimpleDateFormat("yyyy/MM/dd").parse("2022/11/02"),
-            true, true, Template.DATABASE, BackupFrequency.WEEKLY
-        ),
-        VirtualMachine(
-            2, "VM-IT-2",
-            2, 5, 920,
-            SimpleDateFormat("yyyy/MM/dd").parse("2022/12/01"),
-            SimpleDateFormat("yyyy/MM/dd").parse("2022/12/02"),
-            true, true, Template.DATABASE, BackupFrequency.WEEKLY
-        ),
-        VirtualMachine(
-            3, "VM-IT-3",
-            4, 3, 950,
-            SimpleDateFormat("yyyy/MM/dd").parse("2022/12/03"),
-            SimpleDateFormat("yyyy/MM/dd").parse("2022/12/06"),
-            true, true, Template.DATABASE, BackupFrequency.WEEKLY
-        ),
-        VirtualMachine(
-            4, "VM-IT-4",
-            2, 5, 920,
-            SimpleDateFormat("yyyy/MM/dd").parse("2022/12/20"),
-            SimpleDateFormat("yyyy/MM/dd").parse("2022/12/21"),
-            true, true, Template.DATABASE, BackupFrequency.WEEKLY
-        ),
-        VirtualMachine(
-            5, "VM-IT-5",
-            4, 3, 950,
-            SimpleDateFormat("yyyy/MM/dd").parse("2022/12/22"),
-            SimpleDateFormat("yyyy/MM/dd").parse("2022/12/23"),
-            true, true, Template.DATABASE, BackupFrequency.WEEKLY
-        ),
-        VirtualMachine(
-            6, "VM-IT-6",
-            2, 5, 920,
-            SimpleDateFormat("yyyy/MM/dd").parse("2022/12/24"),
-            SimpleDateFormat("yyyy/MM/dd").parse("2022/12/25"),
-            true, true, Template.DATABASE, BackupFrequency.WEEKLY
-        )
-
-    )
+class VoorspellingViewModel(application: Application) : AndroidViewModel(application) {
+    private val database = getDatabase(application)
+    private val virtualMachineRepository = VirtualMachineRepository(database)
+    var vms =  virtualMachineRepository.virtualMachines
 
     private var _voorspelling = MutableLiveData<Voorspelling>()
     val voorspelling: LiveData<Voorspelling>
@@ -63,6 +23,10 @@ class VoorspellingViewModel: ViewModel() {
 
     init {
         _voorspelling.value = Voorspelling( Array(3) { "0" }, Array(3) { "0" })
+        viewModelScope.launch {
+            virtualMachineRepository.refreshVirtualMachines()
+            doeVoorspelling(Date())
+        }
     }
 
     fun doeVoorspelling(datum: Date): Unit{
@@ -79,10 +43,12 @@ class VoorspellingViewModel: ViewModel() {
         var lijst = IntArray(3) { 0 }
 
         var vmlijst: List<VirtualMachine>
+        var vmlist = vms.value ?: listOf()
+
         if (totaal) {
-            vmlijst = vms.filter{ it.startDate <= datum }.toList()
+            vmlijst = vmlist.filter{ it.startDate <= datum }.toList()
         } else {
-            vmlijst = vms.filter { it.startDate <= datum && it.endDate <= datum }.toList()
+            vmlijst = vmlist.filter { it.startDate <= datum && it.endDate <= datum }.toList()
         }
 
         vmlijst.forEach{ v ->
@@ -95,5 +61,15 @@ class VoorspellingViewModel: ViewModel() {
             lijst[2] = tempStorage + v.storage;
         }
         return lijst
+    }
+
+    class Factory(val app: Application) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(VoorspellingViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return VoorspellingViewModel(app) as T
+            }
+            throw IllegalArgumentException("Unable to construct viewmodel")
+        }
     }
 }
