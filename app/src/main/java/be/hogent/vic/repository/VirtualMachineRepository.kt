888d2de3f90d3ce0aaa881df1e6652045ce76460
aglim-benli <aglim.benli@student.hogent.be>
2022-12-18 14:42:44 +0100

    Update VirtualMachineRepository.kt

--- app/src/main/java/be/hogent/vic/repository/VirtualMachineRepository.kt
+++ app/src/main/java/be/hogent/vic/repository/VirtualMachineRepository.kt
@@ -1,25 +1,42 @@
 package be.hogent.vic.repository
 
-import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import be.hogent.vic.database.VicDatabase
+import be.hogent.vic.database.VirtualMachineDatabaseDto
 import be.hogent.vic.database.asDomainModel
-import be.hogent.vic.domain.VirtualMachine
 import be.hogent.vic.network.Network
 import be.hogent.vic.network.asDatabaseModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class VirtualMachineRepository(private val database: VicDatabase) {
-    val virtualMachines: LiveData<List<VirtualMachine>> =
-        Transformations.map(database.virtualMachineDao.getVirtualMachines()) {
-            it.asDomainModel()
-        }
+    private val _virtualMachine = MutableLiveData<VirtualMachineDatabaseDto?>()
+
+    val virtualMachine = Transformations.map(_virtualMachine) {
+        it?.asDomainModel()
+    }
+
+    val virtualMachines = Transformations.map(database.virtualMachineDao.getVirtualMachines()) {
+        it.asDomainModel()
+    }
 
     suspend fun refreshVirtualMachines() {
         withContext(Dispatchers.IO) {
             val virtualMachines = Network.vic.getVirtualMachines()
             database.virtualMachineDao.insertAll(*virtualMachines.asDatabaseModel())
         }
+    }
+
+    suspend fun fetchVirtualMachine(id: Int) {
+        var value: VirtualMachineDatabaseDto?
+
+        withContext(Dispatchers.IO) {
+            val virtualMachine = Network.vic.getVirtualMachine(id)
+            database.virtualMachineDao.update(virtualMachine.asDatabaseModel())
+            value = database.virtualMachineDao.getVirtualMachine(id)
+        }
+
+        _virtualMachine.value = value
     }
 }
