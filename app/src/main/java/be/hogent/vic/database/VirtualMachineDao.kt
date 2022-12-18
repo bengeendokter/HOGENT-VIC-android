9de0eb2ef8dcd6fd9d408088ba015d0828515678
aglim-benli <aglim.benli@student.hogent.be>
2022-12-18 14:42:31 +0100

    Update VirtualMachineDao.kt

--- app/src/main/java/be/hogent/vic/database/VirtualMachineDao.kt
+++ app/src/main/java/be/hogent/vic/database/VirtualMachineDao.kt
@@ -5,11 +5,18 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 
 @Dao
 interface VirtualMachineDao {
     @Query("SELECT * FROM virtual_machines")
     fun getVirtualMachines(): LiveData<List<VirtualMachineDatabaseDto>>
+
+    @Query("SELECT * FROM virtual_machines WHERE id = :id")
+    fun getVirtualMachine(id: Int): VirtualMachineDatabaseDto?
+
+    @Update
+    fun update(vm: VirtualMachineDatabaseDto)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg virtualMachines: VirtualMachineDatabaseDto)
