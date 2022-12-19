07507b0f3158f7c38a19da7534984e4299a8cb53
Yigit Sahin <yigit.sahin@student.hogent.be>
2022-12-20 00:28:19 +0100

    UI testen voor voorspellingFragment (Cpu, Ram en storage)

--- app/src/androidTest/java/be/hogent/vic/screens/voorspelling/VoorspellingFragmentTest.kt
+++ app/src/androidTest/java/be/hogent/vic/screens/voorspelling/VoorspellingFragmentTest.kt
@@ -58,4 +58,83 @@ class VoorspellingFragmentTest {
         //TODO
         // datapicker test en kijken naar datum text en cpu velden
     }
+
+    @Test
+    fun specifiekUICPU() {
+        onView(withId(R.id.cardView)).check(matches(isDisplayed()))
+        onView(withId(R.id.constraint1)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.iconCPU)).check(matches(isDisplayed()))
+        onView(withId(R.id.imageCPU)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.infoCPU)).check(matches(isDisplayed()))
+        onView(withId(R.id.textCPU)).check(matches(isDisplayed()))
+        onView(withId(R.id.textCPU)).check(matches(withText(containsString("CPU"))))
+        onView(withId(R.id.textCPU)).check(matches(withText("CPU")))
+        onView(withId(R.id.textEenheidCPU)).check(matches(withText(containsString("vCPU"))))
+        onView(withId(R.id.textEenheidCPU)).check(matches(withText("in vCPU")))
+
+        onView(withId(R.id.dataCPU)).check(matches(isDisplayed()))
+        onView(withId(R.id.resterend1)).check(matches(isDisplayed()))
+        onView(withId(R.id.resterend1)).check(matches(hasTextColor(R.color.CTA)))
+        onView(withId(R.id.vrijCPU)).check(matches(isDisplayed()))
+        onView(withId(R.id.totaalCPU)).check(matches(isDisplayed()))
+        onView(withId(R.id.totaalCPU)).check(matches(withText(containsString("van"))))
+        /*onView(withId(R.id.voorspelling_btn_datum)).check(matches(withText(containsString("Americano"))))
+
+        onView(withId(R.id.voorspelling_btn_datum)).check(matches(isDisplayed()))
+        onView(withId(R.id.voorspelling_btn_datum)).check(matches(isClickable()))
+        onView(withId(R.id.voorspelling_btn_datum)).check(matches(isNotClickable()))
+        onView(withText("sdf")).check(matches(isNotClickable()))
+        onView(withText("sdf")).check(matches(hasTextColor(R.color.purple_500)))
+        onView(withId(R.id.voorspelling_btn_datum)).check(matches(hasTextColor(Color.BLACK)))*/
+        //Assert.assertEquals(2, 1 + 1)
+    }
+
+    @Test
+    fun specifiekUIRAM() {
+        onView(withId(R.id.cardView2)).check(matches(isDisplayed()))
+        onView(withId(R.id.constraint2)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.iconRAM)).check(matches(isDisplayed()))
+        onView(withId(R.id.imageRAM)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.infoRAM)).check(matches(isDisplayed()))
+        onView(withId(R.id.textRAM)).check(matches(isDisplayed()))
+        onView(withId(R.id.textRAM)).check(matches(withText(containsString("RAM"))))
+        onView(withId(R.id.textRAM)).check(matches(withText("RAM")))
+        onView(withId(R.id.textEenheidRAM)).check(matches(withText(containsString("GB"))))
+        onView(withId(R.id.textEenheidRAM)).check(matches(withText("in GB")))
+
+        onView(withId(R.id.dataRAM)).check(matches(isDisplayed()))
+        onView(withId(R.id.resterend2)).check(matches(isDisplayed()))
+        onView(withId(R.id.resterend2)).check(matches(hasTextColor(R.color.CTA)))
+        onView(withId(R.id.vrijRAM)).check(matches(isDisplayed()))
+        onView(withId(R.id.totaalRAM)).check(matches(isDisplayed()))
+        onView(withId(R.id.totaalRAM)).check(matches(withText(containsString("van"))))
+    }
+
+    @Test
+    fun specifiekUIStorage() {
+        onView(withId(R.id.cardView3)).check(matches(isDisplayed()))
+        onView(withId(R.id.constraint3)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.iconStorage)).check(matches(isDisplayed()))
+        onView(withId(R.id.imageStorage)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.infoStorage)).check(matches(isDisplayed()))
+        onView(withId(R.id.textStorage)).check(matches(isDisplayed()))
+        onView(withId(R.id.textStorage)).check(matches(withText(containsString("Opslag"))))
+        onView(withId(R.id.textStorage)).check(matches(withText("Opslag")))
+        onView(withId(R.id.textEenheidStorage)).check(matches(withText(containsString("GB"))))
+        onView(withId(R.id.textEenheidStorage)).check(matches(withText("in GB")))
+
+        onView(withId(R.id.dataStorage)).check(matches(isDisplayed()))
+        onView(withId(R.id.resterend3)).check(matches(isDisplayed()))
+        onView(withId(R.id.resterend3)).check(matches(hasTextColor(R.color.CTA)))
+        onView(withId(R.id.vrijStorage)).check(matches(isDisplayed()))
+        onView(withId(R.id.totaalStorage)).check(matches(isDisplayed()))
+        onView(withId(R.id.totaalStorage)).check(matches(withText(containsString("van"))))
+    }
+
 }
