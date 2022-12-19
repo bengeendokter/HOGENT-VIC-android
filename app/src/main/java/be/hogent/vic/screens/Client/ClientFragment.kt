25e56903a4a7a94c6d388d6e2a9aef32c976d297
Ben <79012763+benartshogent@users.noreply.github.com>
2022-12-19 15:51:54 +0100

    ClientFragment update

--- app/src/main/java/be/hogent/vic/screens/Client/ClientFragment.kt
+++ app/src/main/java/be/hogent/vic/screens/Client/ClientFragment.kt
@@ -1,60 +1,36 @@
 package be.hogent.vic.screens.Client
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import be.hogent.vic.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import be.hogent.vic.databinding.FragmentClientBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ClientFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ClientFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_client, container, false)
-    }
+        val binding = FragmentClientBinding.inflate(inflater)
+        val selectedId = ClientListFragmentArgs.fromBundle(requireArguments()).selectedId
+        val viewModel = ViewModelProvider(
+            this,
+            ClientViewModel.Factory(requireNotNull(this.activity).application, selectedId)
+        ).get(ClientViewModel::class.java)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ClientFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ClientFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        binding.client.observe(
+            viewLifecycleOwner,
+            Observer {
+                if(it != null){
+                    binding.client = it
                 }
             }
+        )
+        return binding.root
     }
 }
