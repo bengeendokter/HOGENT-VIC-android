4278fea323e059543dbd22cb6cbaac7eb26077fa
Yigit Sahin <yigit.sahin@student.hogent.be>
2022-12-20 00:09:15 +0100

    Viewmodel test toegevoegd voor voorspelling

---
+++ app/src/test/java/be/hogent/vic/screens/voorspelling/VoorspellingViewModelTest.kt
@@ -0,0 +1,12 @@
+package be.hogent.vic.screens.voorspelling
+
+import org.junit.Assert
+import org.junit.Test
+
+class VoorspellingViewModelTest
+{
+    @Test
+    fun test() {
+        Assert.assertEquals(4, 2 + 2)
+    }
+}
