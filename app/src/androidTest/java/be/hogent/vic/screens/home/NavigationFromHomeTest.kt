dcd4a03c090a8eacbd1c47c0a75e375d4302f5b4
Mert Sahin <mert.sahin@student.hogent.be>
2022-12-20 00:46:02 +0100

    navigation test toegevoegd (nieuwe dependency geeft geen errors bij het builden)

---
+++ app/src/androidTest/java/be/hogent/vic/screens/home/NavigationFromHomeTest.kt
@@ -0,0 +1,11 @@
+package be.hogent.vic.screens.home
+
+import org.junit.Assert
+import org.junit.Test
+
+class NavigationFromHomeTest {
+    @Test
+    fun test() {
+        Assert.assertEquals(5, 3 + 2)
+    }
+}
